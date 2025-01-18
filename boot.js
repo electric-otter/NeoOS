a82b87a06855b030ee49dc8c56b4002b7b77e634
electric-otter <electric-otter-github@proton.me>
2025-01-17 20:35:56 -0500

    Update boot.js

--- boot.js
+++ boot.js
@@ -1,3 +1,4 @@
+document.documentElement.requestFullscreen();
 document.onreadystatechange = function() {
     if (document.readyState === 'complete') {
         window.location.href = "https://electric-otter.github.io/NeoOS/desktop.html";
